a5b2998830dd7d805562461c01c2e60d807b73e7
kevanultroneous <dev21.ultroneous@gmail.com>
2022-02-16 17:36:24 +0530

    commit

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,7 +1,7 @@
 const NotFound = () =>{
     return (
         <div>
-            <h1>Not Found</h1>
+            Not found
         </div>
     )
 }
--- src/SystemRoute.js
+++ src/SystemRoute.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import CustomTemplePage from "./Pages/CustomTemplePage"
 import MainPage from "./Pages/MainPage"
+import NotFound from "./Pages/NotFound"
 
 const SystemRoute = () => {
     return (
@@ -8,6 +9,7 @@ const SystemRoute = () => {
             <Routes>
                 <Route path="/" element={<MainPage />} />
                 <Route path="/customtemples" element={<CustomTemplePage />} />
+                <Route path="*" element={<NotFound/>}/>
             </Routes>
         </BrowserRouter>
     )
