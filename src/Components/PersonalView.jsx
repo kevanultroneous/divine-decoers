6b89dac0e0e9242ed4ebc7d76ca194fc7c7e8641
kevanultroneous <dev21.ultroneous@gmail.com>
2022-02-18 19:44:54 +0530

    commit

--- src/Components/PersonalView.jsx
+++ src/Components/PersonalView.jsx
@@ -1,43 +1,34 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Button, Col, Image, Modal, Row } from "react-bootstrap"
 import { AiFillInstagram } from "react-icons/ai"
 import { FaFacebookF } from "react-icons/fa"
 import { customTempleList, personalViewList, sliderRes } from "../common/common"
 import wp from "../Assets/images/wp.svg"
 import Slider from "react-slick/lib/slider"
-// const HoverText = (p) => {
-//     return (
+import plr from "../Assets/images/plr.png";
 
-//         <Modal
-//             show={p.show}
-//             size="lg"
-//             aria-labelledby="contained-modal-title-vcenter"
-//             centered
-//         >
-//             <Modal.Header closeButton>
-//                 <Modal.Title id="contained-modal-title-vcenter">
-//                     Modal heading
-//                 </Modal.Title>
-//             </Modal.Header>
-//             <Modal.Body>
-//                 <h4>Centered Modal</h4>
-//                 <p>
-//                     Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-//                     dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-//                     consectetur ac, vestibulum at eros.
-//                 </p>
-//             </Modal.Body>
+const Models = (p) => {
+    return (
+        <Modal show={p.show} className="modal-v" centered>
+            <Modal.Body>
+                <Image src={p.img} />
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={p.close}>
+                    Close
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    )
+}
 
-//         </Modal>
-//     );
-// };
 const PersonalView = () => {
     const [product_information, setProduct_information] = useState(personalViewList.information)
     const [sliderArray, setSliderArray] = useState(personalViewList.images)
     const [selectedItm, setSelectedItem] = useState('')
-    const [show, setShow] = useState(true)
-
+    const [show, setShow] = useState(false)
     const [hover, setHover] = useState(false)
+    const [img,setImg] = useState('')
     const settings = {
         dots: true,
         infinite: true,
@@ -46,7 +37,10 @@ const PersonalView = () => {
         slidesToScroll: 1,
         responsive: sliderRes
     };
-
+    const onMouseHover = (img) =>{
+        setShow(true)
+        setImg(img)
+    }
     return (
         <>
             {/* {hover && <HoverText show={show} hide={()=>setShow(false)} />} */}
@@ -67,19 +61,24 @@ const PersonalView = () => {
                     <p className="p-font-sub">{personalViewList.price}</p>
                     <p className="p-font-sub2">Availabel : {personalViewList.available ? "In Stock" : "Out of Stock"}</p>
                     <p className="p-sub-para w-75">{personalViewList.description}</p>
+                    <Models 
+                        show={show}
+                        img={img}
+                        close={()=>setShow(false)}
+                    />
                     <p>Select your Frame/Pillar</p>
-
                     <Row>
                         {personalViewList.pillers.map((v) =>
                             <Col xl={2} className="p-0">
-                                <label onMouseOver={() => setHover(true)} onMouseOut={() => { setHover(false) }}>
-                                    <input type="radio" name="test" value={v.img} onChange={(e) => alert(e.target.value)} />
+                                <label onMouseOver={() => onMouseHover(v.img)}>
+                                    <input type="radio" name="test" value={v.img} onChange={(e) => setSelectedItem(e.target.value)} />
                                     <Image src={v.img} />
+                                    <p>{v.nm}</p>
                                 </label>
                             </Col>
                         )}
                     </Row>
-                    <div className=" mt-3 btn btn-primary ">Inquire Now</div>
+                    <Button className=" mt-3 btn btn-primary" disabled={selectedItm ? false : true}>Inquire Now</Button>
                     <div className="mt-4">
                         <label className="fw-bold share">SHARE</label>
                         <label className="ms-3 fb icon-h"><FaFacebookF /></label>
